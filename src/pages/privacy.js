571bb8ca0c44441fa5af8b553562a618622fdb2d
zaf00130 <zflanagan@gmail.com>
2021-01-23 17:08:53 -0500

    legal

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -27,7 +27,7 @@ const IndexPage = () => (
 
 <h2>Privacy Policies</h2>
 
-<P>You may consult this list to find the Privacy Policy for each of the advertising partners of mathisbuilt.</p>
+<p>You may consult this list to find the Privacy Policy for each of the advertising partners of mathisbuilt.</p>
 
 <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on mathisbuilt, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
 
